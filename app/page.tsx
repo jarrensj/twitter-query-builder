2cb6e1ca1a50545b2305588ebeac98275b48f9cb
Jarren San Jose <sanjose.jarren@gmail.com>
2024-06-29 23:42:35 -0700

    add title and description to page

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,12 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center p-24">
-      <h1 className="text-4xl font-bold text-center">
-        hi
+      <h1 className="text-2xl font-bold text-center">
+        twitter query builder
       </h1>
+      <p className="text-center">
+        A tool to help you search for tweets on Twitter.
+      </p>
     </main>
   );
 }
